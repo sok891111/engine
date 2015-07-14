24a6d5e64278d098a24e46a4de71f3875b8cb7d9
Your Name <sok891111@gmail.com>
2015-07-14 20:57:18 +0900

    reverse

--- http/ssl_test.py
+++ http/ssl_test.py
@@ -1,14 +1,25 @@
-import socket
-try:
-    import ssl
-except ImportError:
-    pass
+"""Spawn multiple workers and wait for them to complete"""
+# from __future__ import print_function
+import sys
+from httphandler import Response
+urls = [ 'https://www.slack.com','http://www.naver.com','http://www.daum.net']
 
-# context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-# context.verify_mode = ssl.CERT_REQUIRED
-# context.check_hostname = True
-# context.load_verify_locations("/etc/ssl/certs/ca-certificates.crt")
-conn = ssl.SSLSocket(socket.socket(socket.AF_INET))
-conn.connect(("www.ibm.com\\/us\\/en", 443))
-conn.sendall("HEAD / HTTP/1.1\r\nHost: www.ibm.com\\/us\\/en\r\n\r\n")
-print conn.recv(8024)
+import gevent
+from gevent import monkey
+
+# patches stdlib (including socket and ssl modules) to cooperate with other greenlets
+monkey.patch_all()
+from urllib2 import urlopen
+
+def print_head(url):
+    print 'Starting %s' % url
+    try:
+        res = Response(urlopen(url))
+    except Exception, why:
+        print why 
+        return
+    print "%s %s"%(res.request_url,res.getheader("server")) 
+
+jobs = [gevent.spawn(print_head, url) for url in urls]
+
+gevent.wait(jobs)
